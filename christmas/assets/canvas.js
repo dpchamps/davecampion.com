24eac50205e11c98d1ad5746fb00739d75d45940
David Campion <gasolinewaltz@gmail.com>
2014-12-19 21:47:33 -0800

    made it a little more pretty.

--- christmas/assets/canvas.js
+++ christmas/assets/canvas.js
@@ -6,27 +6,52 @@
 //the canvas basics
 var canvas = document.getElementById("canvas"),
     ctx = canvas.getContext('2d'),
+    particleCtx = document.getElementById("particles").getContext("2d");
     w=0,
     h=0,
     renderArray = [],
+    images = {},
+    audio = [],
     animFrame = null,
-    pattern = new Image(),
-    audio;
+    date = new Date(2014,11, 25);
 
 
+function loadImages(sources, callback){
+    var imgsLoaded = 0,
+        numImages = 0;
+
+    for(var src in sources){
+        if(sources.hasOwnProperty(src)){
+            numImages++;
+            console.log(imgsLoaded, numImages, images);
+            images[src] = new Image();
+            images[src].onload = (function(){
+                if(++imgsLoaded >= numImages) {
+                    callback();
+                }
+            });
+            images[src].src = sources[src];
+        }
+    }
+}
 function loadResources(){
-    audio = new Audio('assets/music/99 1 AM (Christmas).mp3');
-    audio.onloadeddata = function(){
-        audio.play();
-        //start playing audio first, then load the patter, then start the particles
-        pattern.src = 'assets/images/snow.png';
-        pattern.onload = function(){
-            renderArray.push(cluster);
-            animationLoop();
-        };
+    var imagePaths = {
+        pattern : './assets/images/snow.png',
+        bulletin : './assets/images/bulletin1.png',
+        volume: './assets/images/volume-mute2.png'
     };
+    audio = new Audio('assets/music/99 1 AM (Christmas).mp3');
+    audio.onloadeddata = function() {
+        audio.play();
+        loadImages(imagePaths, function(){
+            console.log(images);
+            renderArray.push(cluster);
+            resizeCanvas();
+            animationLoop();
+        });
+        audio.loop = true;
+    }
 
-    audio.loop = true;
 }
 function toggleAudio(){
     var speaker = document.getElementById('toggle-audio');
@@ -41,7 +66,8 @@ function toggleAudio(){
 function resizeCanvas(){
     ctx.canvas.width  = window.innerWidth;
     ctx.canvas.height = window.innerHeight;
-
+    particleCtx.canvas.width = window.innerWidth;
+    particleCtx.canvas.height = window.innerHeight;
     w = canvas.width;
     h = canvas.height;
     cluster.origin = {
@@ -49,11 +75,15 @@ function resizeCanvas(){
         y: -70
     };
     cluster.degradeRate = Math.sqrt((w*w)+(h*h));
+
+    drawSky();
+    drawGround();
 }
 function animationLoop(){
     animFrame = requestAnimationFrame(animationLoop);
     if(renderArray.length > 0){
-        ctx.clearRect(0,0,w,h);
+
+        particleCtx.clearRect(0,0,w,h);
         for(var i = 0; i < renderArray.length; i++){
             render(renderArray[i]);
         }
@@ -68,22 +98,24 @@ function drawSky(){
     ctx.fillStyle=grd;
     ctx.fillRect(0,0,w,h*7);
 }
+
 function drawGround(){
-    ctx.fillStyle = ctx.createPattern(pattern, 'repeat');
+    ctx.fillStyle = ctx.createPattern(images.pattern, 'repeat');
     ctx.save();
     ctx.beginPath();
     ctx.moveTo(0, h);
-    ctx.bezierCurveTo((w/2)-250, h*0.7,(w/2)+250,h*0.7,w, h)
+    ctx.bezierCurveTo((w/2)-250, h*0.7,(w/2)+250,h*0.7,w, h);
     ctx.closePath();
     ctx.fill();
     ctx.restore();
 }
 function render(cluster){
 
-    drawSky();
-    drawGround();
+    //drawSky();
+    //drawGround();
+   // drawBulletin();
     cluster.emitter();
-    cluster.step(ctx);
+    cluster.step(particleCtx);
 
 }
 //instantiate the particle engine;
@@ -117,7 +149,8 @@ cluster.emitter = (function(){
     }
 });
 
-resizeCanvas();
+
+
 loadResources();
 window.addEventListener('resize', resizeCanvas);
 document.getElementById('toggle-audio').addEventListener('click', toggleAudio);
